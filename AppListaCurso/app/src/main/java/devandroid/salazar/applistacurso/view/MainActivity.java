f15e0ef806c6e74693adf8ac2753b0e801cdfd83
Salazar <diogosalazar@hotmail.com>
2023-04-01 00:28:21 -0300

    Layout correto App Lista Vip, TextView e EditText.

--- AppListaCurso/app/src/main/java/devandroid/salazar/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/salazar/applistacurso/view/MainActivity.java
@@ -3,6 +3,7 @@ package devandroid.salazar.applistacurso.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import devandroid.salazar.applistacurso.R;
 import devandroid.salazar.applistacurso.model.Pessoa;
@@ -10,6 +11,7 @@ import devandroid.salazar.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa1;
+    Pessoa pessoa2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +20,19 @@ public class MainActivity extends AppCompatActivity {
 
 
         pessoa1 = new Pessoa();
+        pessoa1.setPrimeiroNome("Diogo");
+        pessoa1.setSobrenome("Araujo");
+        pessoa1.setCursoDesejado("Android");
+        pessoa1.setTelefoneContato("21-22443355");
+
+        pessoa2 = new Pessoa();
+        pessoa2.setPrimeiroNome("Alyne");
+        pessoa2.setSobrenome("Coelho");
+        pessoa2.setCursoDesejado("Cozer");
+        pessoa2.setTelefoneContato("24-99896542");
+
+        Log.i("POO", pessoa1.toString());
+        Log.i("POO", pessoa2.toString());
+
     }
 }
